d7ee433dab17ae9227f391b0170efa541a24c495
wbeiyer <wbeiyer@163.com>
2024-10-28 21:07:12 +0800

    delete .sh

--- pix.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-export CUDA_VISIBLE_DEVICES=0
-python train.py --dataroot ./datasets/thread --name thread_pix2pix --model pix2pix --dataset_mode aligned --direction AtoB
-
-# python train.py --dataroot ./datasets/thread --name thread_pix2pix --model pix2pix --dataset_mode template > output.log 2>&1
-# python test.py --dataroot ./datasets/thread --name thread_pix2pix --model pix2pix --dataset_mode template > output1.log 2>&1
